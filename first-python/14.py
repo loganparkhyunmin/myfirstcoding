2b9ce12e1dbb578cf189afa0a743402d759e89ca
Logan Park <logan050530@gmail.com>
2023-07-16 14:24:26 +0900

    Updatel list content

---
+++ first-python/14.py
@@ -0,0 +1,44 @@
+a = []
+a.append(10)
+print(a)
+
+b=['a','b','c']
+b.append('d')
+print(b)
+
+line_up =[]
+line_up.append('거북이')
+line_up.append('홍학')
+line_up.append('토끼')
+print(line_up)
+
+print("=====================")
+
+c=[1,2,4,5]
+c.insert(2,3)
+print(c)
+
+d=[3,1,2,3]
+d.remove(3)
+print(d)
+
+a=[6,2,4,1]
+a.sort()
+print(a)
+
+b=['carrot','apple','banana']
+b.sort()
+print(b)
+
+line_up=['거북이','홍학','토끼']
+line_up.insert(1,"도도새") 
+print(line_up)
+
+line_up=['거북이', '도도새', '홍학', '토끼']
+line_up.remove('도도새')
+print(line_up)
+
+line_up=['거북이','홍학','토끼']
+line_up.sort()
+print(line_up)
+
